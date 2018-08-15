d88643b8a803a409ba9a64a58c1edab37c400fd4
Jean-Francois Larouche <rouche@gmail.com>
2018-08-14 21:55:06 -0400

    Environment

--- src/app/components/hellomars/hellomars.ts
+++ src/app/components/hellomars/hellomars.ts
@@ -3,5 +3,9 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class HelloMars extends Vue {
+
+    public serviceUrl: string = process.env.VUE_APP_CONFIG_SERVICE_URL || '';
+
     @Prop() private msg!: string;
+
 }
